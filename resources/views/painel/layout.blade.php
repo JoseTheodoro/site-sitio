fd799a9d7d194b21d46514732dbbe0c7cdd058fa
Jose Theodoro <jose.theodoro42@gmail.com>
2018-10-29 19:08:49 -0300

    Update layout.blade.php

--- resources/views/painel/layout.blade.php
+++ resources/views/painel/layout.blade.php
@@ -13,8 +13,11 @@
 
         <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{route('dash')}}">Jardim dos Vagalumes</a>
-                <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+  
                 <ul class="navbar-nav px-3">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
                   <li class="nav-item text-nowrap">
                   <a class="nav-link" href="{{route('logout')}}"><span data-feather="user-x"></span> Sair</a>
                   </li>
@@ -24,7 +27,7 @@
               <div class="container-fluid">
                 <div class="row">
                   <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-                    <div class="sidebar-sticky">
+                    <div class="sidebar-sticky" id="navbarCollapse">
                       <ul class="nav flex-column">
                         <li class="nav-item">
                         <a class="nav-link" href="{{route('users')}}">
