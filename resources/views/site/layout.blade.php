72d6834256b492fe05892e0d02e008572e411460
Jose Theodoro <jose.theodoro42@gmail.com>
2018-10-29 19:04:54 -0300

    Update layout.blade.php

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -36,7 +36,7 @@
             <a class="nav-link" href={{route('fotos')}}>Fotos</a>
             </li>
              <li class="nav-item">
-             <a class="nav-link" href={{route('gastronomia')}}>Gastronomia</a>
+             <a class="nav-link" href={{route('gastronomia')}}>Cardápios</a>
             </li>
             <li class="nav-item">
             <a class="nav-link" href={{route('localizacao')}}>Localização</a>
