ec533928cd2399ab6714bb5b05addcdb2eb8a3da
Jose Theodoro <jose.theodoro@jsl..com.br>
2019-06-21 11:21:13 -0300

    table reservas

--- resources/views/painel/eventos/reserva.blade.php
+++ resources/views/painel/eventos/reserva.blade.php
@@ -13,35 +13,37 @@
 
 <div class="row">
     <div class="col-sm-12">
-        <table class="table table-sm table-striped">
-                <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Email</th>
-                            <th>Telefone</th>
-                            <th>Qtde. Pessoas</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                
-                    <tbody>
-                        @forelse($evento->confirm as $confirm)
-                        <tr>
-                            <td>{{$confirm->name}}</td>
-                            <td>{{$confirm->email}}</td>
-                            <td>{{$confirm->phone}}</td>
-                            <td>{{$confirm->number}}</td>
-                            <td>
-                                <a href="{{route('cancela.reserva', $confirm->id)}}"> Cancelar</a>
-                            </td>
-                        </tr>
-                        @empty
-                            <tr>
-                                <td colspan="5">Nenhuma reserva</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-        </table>
+      <div class="table-responsive">
+          <table class="table table-sm table-striped">
+                  <thead>
+                          <tr>
+                              <th>Nome</th>
+                              <th>Email</th>
+                              <th>Telefone</th>
+                              <th>Qtde. Pessoas</th>
+                              <th></th>
+                          </tr>
+                      </thead>
+
+                      <tbody>
+                          @forelse($evento->confirm as $confirm)
+                          <tr>
+                              <td>{{$confirm->name}}</td>
+                              <td>{{$confirm->email}}</td>
+                              <td>{{$confirm->phone}}</td>
+                              <td>{{$confirm->number}}</td>
+                              <td>
+                                  <a href="{{route('cancela.reserva', $confirm->id)}}"> Cancelar</a>
+                              </td>
+                          </tr>
+                          @empty
+                              <tr>
+                                  <td colspan="5">Nenhuma reserva</td>
+                              </tr>
+                          @endforelse
+                      </tbody>
+          </table>
+      </div>
     </div>
 </div>
 
